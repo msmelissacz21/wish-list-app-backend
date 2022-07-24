9a14ced87b063d47d54a274dbeb190d8f899e724
Melissa Zuengler <melissazuengler@Melissas-MacBook-Pro.local>
2022-07-24 10:55:33 -0500

    Added favorites controller

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ let express = require('express')
 // Initialize the app object
 let app = express()
 
+app.use('/places', require('./controllers/favorites'))
+
 // Home page route
 app.get('/', (req,res) => {
     res.send('Hello Home Page')
 })
 
+app.get('*', (req,res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
 
 // Listen for connections
-app.listen(4050, function () {
+app.listen(process.env.PORT, function () {
     console.log('Listening on port 4050!')
 })
