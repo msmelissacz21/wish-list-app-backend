2305aed2cbd0f3108c3e41e880542de429987a09
Melissa Zuengler <melissazuengler@Melissas-MacBook-Pro.local>
2022-07-24 10:34:40 -0500

    Set up express, displaying on brower

---
+++ README.md
@@ -0,0 +1,1 @@
+The purpose of this application is to add, delete, and prioitize items on wish.  This is the back end code of the application.
--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log(index)
+// Require needed modules
+let express = require('express')
+
+// Initialize the app object
+let app = express()
+
+// Home page route
+app.get('/', (req,res) => {
+    res.send('Hello Home Page')
+})
+
+
+// Listen for connections
+app.listen(4050, function () {
+    console.log('Listening on port 4050!')
+})
