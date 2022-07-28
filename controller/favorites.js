c4950f1e4305b153ba5b5b88613ff2b04fe4d183
Melissa Zuengler <melissazuengler@Melissas-MacBook-Pro.local>
2022-07-28 18:02:34 -0500

    More development of routes

--- controller/favorites.js
+++ controller/favorites.js
@@ -14,6 +14,7 @@ router.get('/', async (req,res) => {
     }
 })
 
+
 // POST is used to create a new entity
 router.post('/save', async (req, res) => {
     try {
@@ -26,25 +27,58 @@ router.post('/save', async (req, res) => {
     }
 })
 
-router.post('/user-favorites/user-id/:userId', async (req, res) => {
-    db.UserFavorites.create({userId: req.params.userId, favorites: []}).then()
-    res.send()
+
+// GET is used to retrieve data
+router.get('/user-favorites/name/:name', async (req, res) => {
+    try {
+        f = await db.UserFavorites.find({name: req.params.name}).then()
+        res.send(f)
+    } catch (err) {
+        if (`{:name}` === undefined ) {
+            console.log('Name does not exist')
+        } else {
+            console.log(err)
+            res.status(500).json({ 'message': ''})
+        }
+    }
 })
+
+
+// POST is used to create a new entity
+router.post('/user-favorites/name/:name', async (req, res) => {
+    try {
+        db.UserFavorites.create({name: req.params.name, favorites: []}).then()
+        res.send()
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ 'message': 'Something went wrong'}) 
+    }
+})
+
 
 // PUT method is used to update an existing entity
-router.put('/user-favorites/user-id/:userId', async (req, res) => {
-    db.UserFavorites.findOneAndUpdate({userId: req.params.userId, favorites: req.body}).then()
-    res.send()
+router.put('/user-favorites/name/:name', async (req, res) => {
+    try {
+        db.UserFavorites.findOneAndUpdate({name: req.params.name}, {favorites: req.body}).then()
+        res.send()
+    } catch (err) {
+        if (`{:name}` === undefined ) {
+            console.log('Name does not exist')
+        } else {
+            console.log(err)
+            res.status(500).json({ 'message': 'Something went wrong'})
+        }
+    }
 })
 
-router.get('/user-favorites/user-id/:userId', async (req, res) => {
-    f = await db.UserFavorites.find({userId: req.params.userId}).then()
-    res.send(f)
-})
 
 //  DELETE method 
-router.delete('/:id', (req,res) => {
+router.delete('/user-favorites/name/:name', (req,res) => {
+    try {
+        db.UserFavorites.findOneAndDelete( {name: req.params.name}, {fav} )
+    } catch (err) {
 
+    }
 })
 
 module.exports = router
--- model/user-favorites.js
+++ model/user-favorites.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const userFavorites = new mongoose.Schema({
-    userId: { type: String, required: true},
+    name: { type: String, required: true},
     favorites: { type: [], required: false}
 })
   
