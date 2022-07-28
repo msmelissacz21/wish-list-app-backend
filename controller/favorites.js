72209ee06e4e18aadeb72e9b6a14e79d1b2ffa27
Melissa Zuengler <melissazuengler@Melissas-MacBook-Pro.local>
2022-07-28 18:42:52 -0500

    Changing routes again

--- controller/favorites.js
+++ controller/favorites.js
@@ -1,33 +1,6 @@
 const router = require('express').Router()
 const db = require('../model')
 
-
-// GET is used to retrieve data
-router.get('/', async (req,res) => {
-    try {
-        let favorites = await db.Favorites.find()
-        res.send(favorites)
-
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({ 'message': 'unable to retrieve favorites'})
-    }
-})
-
-
-// POST is used to create a new entity
-router.post('/save', async (req, res) => {
-    try {
-        await db.Favorites.create(req.body).then(
-        console.log('Saved to wishlist'))
-        res.send()
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({ 'message': ''})
-    }
-})
-
-
 // GET is used to retrieve data
 router.get('/user-favorites/name/:name', async (req, res) => {
     try {
